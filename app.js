a6fbce7d2a20b52ebf433a1bf40f6b639f8c745c
Karuna sagar <karunasagar129130@gmail.com>
2024-03-31 23:38:45 +0530

    Change variable name for port and add default port from environment variable
    * Renamed `const port = 3000;` to `const port = process.env.PORT || 3000;`
    * Added environment variable for Mongo URI in connectDB function call.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require ('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
@@ -17,7 +17,7 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
